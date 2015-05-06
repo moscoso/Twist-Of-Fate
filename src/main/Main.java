3d09eafadc1ead16d297af6a0189cd854be1c47d
Chris Moscoso <christophermoscoso@gmail.com>
2015-05-06 19:38:04 -0400

    Initializing the repository for new RPG Game

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+
+package main;
+
+/**
+ *
+ * @author ChrisMoscoso
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("This will be the start of an epic game!");
+    }
+}
